c157d690e2f45c09037b022c9c42b260e1958752
Firas Al-Houssari <f.alhusari@gmail.com>
2019-06-01 00:51:12 +0300

    size for small and big screen

--- UniHelp/UniHelp/ClientApp/src/components/Post.js
+++ UniHelp/UniHelp/ClientApp/src/components/Post.js
@@ -1,50 +1,19 @@
 ﻿import React, { Component } from 'react';
+import './styles.css'
 
 export class Post extends Component {
 
     render() {
         return (
-            <div width="100%" height="30%" max-height="100" style={divStyle}>
-                <p style={authorStyle}>{this.props.author}</p>
-                <hr style={lineStyle} />
-                <p style={titleStyle}>{this.props.title}</p>
-                <p>{this.props.content}</p>
-                <img src={this.props.image} style={imageStyle} ></img>
+            <div width="100%" height="30%" max-height="100" className="postDiv">
+                <p className="authorStyle">{this.props.author}</p>
+                <hr className="lineStyle" />
+                <p className="titleStyle">{this.props.title} </p>
+                <p classname="contentStyle">{this.props.content}</p>
+                <img src={this.props.image} className="imageStyle"></img>
             </div>
         );
     }
-}
-
-const divStyle = {
-    border: '1px solid lightgray',
-    borderRadius: '10px',
-    padding: '5px',
-    marginTop: '5px'
-};
-
-const imageStyle = {
-    height: '60%',
-    width: '100%',
-    borderRadius: '5px',
-    objectFit: 'cover'
-};
-
-const titleStyle = {
-    fontWeight: 'bold'
-};
-
-const contentStyle = {
-
-};
-
-const authorStyle = {
-    fontWeight: 'bold',
-    marginTop: '5px',
-    marginLeft: '5px'
-};
-
-const lineStyle = {
-    marginTop: '0px'
 }
 
 export default Post
